a6888d420c7a4537f17fc0ae44a9c1fb2b734ec9
saurav_kothale <sauravkothale@gmail.com>
2024-03-23 13:25:22 +0530

    Update : Update the Callable funtion

--- app/setting.py
+++ app/setting.py
@@ -1,6 +1,7 @@
 from decouple import config
 import json
 
+
 deployment_env = config('DEPLOYMENT_ENV', default='production')
 
 if deployment_env == 'local':
@@ -19,18 +20,18 @@ if deployment_env == 'local':
 else:
     # Retrieve credentials from AWS Secrets Manager
     import boto3
-    client = boto3.client( service_name='secretsmanager',region_name="us-east-1")
+    client = boto3.client(service_name='secretsmanager',region_name="us-east-1")
     response = client.get_secret_value(SecretId="prod")
     secret_data = response['SecretString']
     secrets = json.loads(secret_data)
     SECRET_KEY = secrets['SECRET_KEY']
-    ALGORITHAM = secrets('ALGORITHAM')
-    DB_USER_NAME = secrets("DB_USER_NAME")
+    ALGORITHAM = secrets['ALGORITHAM']
+    DB_USER_NAME = secrets["DB_USER_NAME"]
     DB_PASSWORD = secrets("DB_PASSWORD")
-    DB_PORT = secrets("DB_PORT")
-    DB_NAME = secrets("DB_NAME")
-    DB_HOST = secrets("DB_HOST")
-    AWS_ACCESS_KEY = secrets("AWS_ACCESS_KEY")
-    AWS_SECRET_KEY = secrets("AWS_SECRET_KEY")
-    ROW_BUCKET = secrets("ROW_BUCKET")
-    PROCESSED_FILE_BUCKET = secrets("PROCESSED_FILE_BUCKET")
+    DB_PORT = secrets["DB_PORT"]
+    DB_NAME = secrets["DB_NAME"]
+    DB_HOST = secrets["DB_HOST"]
+    AWS_ACCESS_KEY = secrets["AWS_ACCESS_KEY"]
+    AWS_SECRET_KEY = secrets["AWS_SECRET_KEY"]
+    ROW_BUCKET = secrets["ROW_BUCKET"]
+    PROCESSED_FILE_BUCKET = secrets["PROCESSED_FILE_BUCKET"]
