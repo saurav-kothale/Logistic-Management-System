20590ce51c7e07c73171a8148266c558c787c94d
saurav-kothale <sauravkothale@gmail.com>
2024-06-06 12:48:45 +0530

    Update : Update Column Field

--- app/salary_surat/view/zomato_structure2.py
+++ app/salary_surat/view/zomato_structure2.py
@@ -187,14 +187,14 @@ def calculate_bike_charges_for_rental_model(
         amount = vahicle_charges_fulltime
 
     elif (
-        job_type == "part Time"
+        job_type == "part time"
         and average <= partime_average
         and orders <= partime_greter_than_order
     ):
         amount = vahicle_charges_partime
     
     elif (
-        job_type == "part Time"
+        job_type == "part time"
         and average <= partime_average
         and orders >= partime_greter_than_order
     ):
